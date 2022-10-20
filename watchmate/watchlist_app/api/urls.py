c3732fef796d6411c9b7388901549aafdf323c6d
RoufDs <four.3687zero@gmail.com>
2022-10-20 19:35:58 +0700

    GenericAPIView have HTML Form

--- watchmate/watchlist_app/api/urls.py
+++ watchmate/watchlist_app/api/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 # from .views import movie_list, movie_details
-from .views import WatchListAV, WatchDetailAV, StreamPlatformAV, StreamPlatformDetailAV
+from .views import ReviewDetail, ReviewList, WatchListAV, WatchDetailAV, StreamPlatformAV, StreamPlatformDetailAV
 
 urlpatterns = [
     path('list/', WatchListAV.as_view(), name='movie-list'),
     path('<int:pk>/', WatchDetailAV.as_view(), name='movie-detail'),    
     path('stream/', StreamPlatformAV.as_view(), name='stream'),
     path('stream/<int:pk>/', StreamPlatformDetailAV.as_view(), name='streamplatform-detail'),
+    
+    path('review/', ReviewList.as_view(), name='review-list'),
+    path('review/<int:pk>/', ReviewDetail.as_view(), name='review-detail'),
 ]
